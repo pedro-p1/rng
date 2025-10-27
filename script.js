9075b8606183abd6e8724f4a37e9e376941e7126
pedro-p1 <pedro.elzein@gmail.com>
2025-10-27 22:06:40 +0000

    2 digit fully functional

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ let digits = 2
 const display = document.getElementById("generate")
 
 function generate(){
-    if digits == 2 {
+    if (digits == 2) {
         number = Math.floor(Math.random() * (99 - 10 + 1)) + 10
     }
-    if digits == 3 {
+    if (digits == 3) {
         number = Math.floor(Math.random() * (999 - 100 + 1)) + 100
     }
 
