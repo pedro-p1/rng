0cce23c3abbac63fffa3487c27af6e99a91b9b40
pedro-p1 <pedro.elzein@gmail.com>
2025-10-27 22:01:54 +0000

    added some js

---
+++ script.js
@@ -0,0 +1,14 @@
+let number = 0
+let digits = 2
+const display = document.getElementById("generate")
+
+function generate(){
+    if digits == 2 {
+        number = Math.floor(Math.random() * (99 - 10 + 1)) + 10
+    }
+    if digits == 3 {
+        number = Math.floor(Math.random() * (999 - 100 + 1)) + 100
+    }
+
+    display.innerText = number;
+}
